fcd81fa2e06e4f9371a8c03bf7445a9da60d3063
Victor Tran <victortran415@gmail.com>
2020-10-15 11:15:38 -0700

    must contain criteria

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var lowLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 
 var UppLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+
 function createPassword() {
   var lengthOfPw = parseInt(prompt("How long would you like your password to be? Must be between 8 & 128 characters"));
 
@@ -15,20 +16,19 @@ function createPassword() {
     alert("Must have a value");
 
   } else if (lengthOfPw < 8 || lengthOfPw > 128) {
-    lengthOfPw = parseInt(prompt("The number you entered must be from 8 to 128"));   
+    lengthOfPw = parseInt(prompt("Must be in the range between 8 and 128 characters"))
+
+  } else {
+    var wantSymbols = confirm("Would you like SYMBOLS in your password?")
+    var wantNum = confirm("Would you like NUMBERS in your password?")
+    var wantUpperLet = confirm("Would you like UPPERCASE LETTER in your password?")
+    var wantLowerLet = confirm("Would you like LOWERCASE LETTERS in your password?")
+
+    if (wantSymbols === false && wantNum === false && wantUpperLet === false && wantLowerLet === false) {
+      alert("Password must contain at least ONE criteria")
+    }
   }
-  var wantSymbols = confirm("Would you like SYMBOLS in your password?");
-  var wantNum = confirm("Would you like NUMBERS in your password?");
-  var wantUpperLet = confirm("Would you like UPPERCASE LETTER in your password?");
-  var wantLowerLet = confirm("Would you like LOWERCASE LETTERS in your password?");
-
-  if (!wantSymbols && !wantNum && !wantLowerLet && !wantUpperLet) {
-  alert("Must contain at least one criteria");
-  } 
 }
-
-
-
 
 // Write password to the #password input
 // function writePassword() {
@@ -39,9 +39,10 @@ function createPassword() {
 // }
 
 function copyToClipboard() {
-  document.getElementById("password").select();
-  alert("Your password has now been copied to the clipboard");
+  document.getElementById("password");
+  alert("Your password has been copied to clipboard")
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", createPassword);
 copyPassBtn.addEventListener("click", copyToClipboard)
